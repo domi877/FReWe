8947af78b634071bbc57326816b903b6304e01c4
domi877 <domiiffland@yahoo.de>
2020-08-08 22:25:07 +0200

    Clean App.js file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
+import "./App.css";
 
-// that is a component
-function App() {
-  const sayHello = () => {
-    console.log("hello Dominik");
-  };
-
-  return (
-    // this is not real HTML, it is Javascript (jsx)
-    // React compiles this JS to real HTML => (React.createElement(div))
-    <div>
-      <h1>Hello World!</h1>
-      {/* onClick='temp' equals HTML, onClick={temp} equals Javascript */}
-      <button onClick={sayHello}>Hello</button>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
