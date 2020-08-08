cbb67af36d856e63f0cdbe4a1cf554689d9bf58e
domi877 <domiiffland@yahoo.de>
2020-08-08 16:10:19 +0200

    Create Hello World

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+// that is a component
 function App() {
+  const sayHello = () => {
+    console.log("hello Dominik");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // this is not real HTML, it is Javascript (jsx)
+    // React compiles this JS to real HTML => (React.createElement(div))
+    <div>
+      <h1>Hello World!</h1>
+      {/* onClick='temp' equals HTML, onClick={temp} equals Javascript */}
+      <button onClick={sayHello}>Hello</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
+// render the App
+// Javascript renders everything in a single div in index.html
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
